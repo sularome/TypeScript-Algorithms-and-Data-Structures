b800474109fce5af60b7d40724f0332aeb95579f
sularome <sularome@gmail.com>
2016-09-02 22:59:15 +0200

    Fix typed Queue

--- tests/TypedQueueSpec.js
+++ tests/TypedQueueSpec.js
@@ -1,13 +1,20 @@
 fdescribe('Queue', function () {
     describe('enqueue', function () {
         it('should add elements to the stack', function () {
-            var q = new TypedQueue(new Uint8Array(2));
+            var q = new TypedQueue(new Uint8Array(3));
             q.enqueue(1);
+            expect(q.size()).toEqual(1);
+            expect(q.peek()).toEqual(1);
+            q.enqueue(2);
+            expect(q.peek()).toEqual(1);
+            expect(q.size()).toEqual(2);
+             q.dequeue();
+            expect(q.peek()).toEqual(2);
             expect(q.size()).toEqual(1);
             q.enqueue(2);
             expect(q.size()).toEqual(2);
-             /*q.dequeue();
-            expect(q.size()).toEqual(1);*/
+            q.dequeue();
+            expect(q.size()).toEqual(1);
         });
     });
 });
--- ts/TypedQueue.ts
+++ ts/TypedQueue.ts
@@ -3,21 +3,20 @@ class TypedQueue {
     protected queue: ITypedArray;
     protected head:number = 0;
     protected tail:number = 0;
-    protected length:number = 0;
-    protected  MAX_CLEAR_QUEUE_SIZE = 1000;
+    protected memoryLength:number = 0;
 
     constructor (queue:ITypedArray) {
         this.queue = queue;
-        this.length = queue.length;
+        this.memoryLength = queue.length;
     }
 
     public enqueue (element:number) {
-        if (this.size() === this.length) {
+        if (this.size() === this.memoryLength - 1) {
             throw new RangeError('The queue is full');
         }
         this.queue[this.tail] = element;
         this.tail++;
-        if (this.tail === this.length) {
+        if (this.tail === this.memoryLength) {
             this.tail = 0;
         }
     }
@@ -28,7 +27,7 @@ class TypedQueue {
         }
         var  element = this.queue[this.head];
         this.head++;
-        if (this.head === this.length) {
+        if (this.head === this.memoryLength) {
             this.head = 0;
         }
         return element;
@@ -38,8 +37,14 @@ class TypedQueue {
         return this.tail === this.head;
     }
 
+    public peek ():number {
+        if (this.isEmpty()) {
+            throw new Error('The stack is empty');
+        }
+        return this.queue[this.head];
+    }
+
     public size () {
-        console.log((this.tail < this.head) ? this.tail + this.length - this.head : this.tail - this.head);
-        return (this.tail < this.head) ? this.tail + this.length - this.head : this.tail - this.head;
+        return (this.tail < this.head) ? this.tail + this.memoryLength - this.head : this.tail - this.head;
     }
 }
