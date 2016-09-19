4ff27384dca4de4f43431b9133380b2b44c33eb9
Yordan Stoev <sularome>
2016-09-19 23:16:14 +0200

    Add BitSet

---
+++ tests/BitSetSpec.js
@@ -0,0 +1,31 @@
+describe('BitSet', function () {
+    it('be able to set bits', function () {
+        var bs = new BitSet(100);
+        bs.set(1, true);
+        bs.set(99, true);
+        expect(bs.get(0)).toEqual(false);
+        expect(bs.get(1)).toEqual(true);
+        expect(bs.get(54)).toEqual(false);
+        expect(bs.get(99)).toEqual(true);
+    });
+
+    it('be able to get set bits count', function () {
+        var bs = new BitSet(100);
+        bs.set(1, true);
+        bs.set(99, true);
+        expect(bs.count()).toEqual(2);
+        bs.set(99, false);
+        expect(bs.count()).toEqual(1);
+    });
+
+    it('be able to get set bits count', function () {
+        var bs = new BitSet(100);
+        bs.set(1, true);
+        bs.set(99, true);
+        expect(bs.getIndexes()).toEqual([1,99]);
+        bs.set(29, true);
+        expect(bs.getIndexes()).toEqual([1,29,99]);
+        bs.set(29, false);
+        expect(bs.getIndexes()).toEqual([1,99]);
+    });
+});
---
+++ ts/BitSet.ts
@@ -0,0 +1,62 @@
+class BitSet {
+    private length:number = 0;
+    private buffer:Uint32Array;
+
+    constructor (size:number) {
+        this.length = size;
+        this.buffer = new Uint32Array(Math.ceil(this.length / 32) * 4);
+    }
+
+    public count() {
+        return this.buffer.reduce((total, chunk) => this.bitsCountInNumber(chunk) + total, 0);
+    }
+
+    public get(index:number) {
+        this.validateIndex(index);
+        const chunkOffset = Math.floor(index / 32);
+        const bitOffset = index - chunkOffset * 32;
+        return !! (this.buffer[chunkOffset] & (1 << bitOffset));
+    }
+
+    public getIndexes() {
+        let result:number[] = [];
+        let bitIndex:number = 0;
+        let bit:boolean = false;
+        while (bitIndex < this.length) {
+            bit = this.get(bitIndex);
+            if (bit) {
+                result.push(bitIndex);
+            }
+            bitIndex++;
+        }
+        return result;
+    }
+
+    public size() {
+        return this.length;
+    }
+
+    public set(index:number, value: boolean) {
+        this.validateIndex(index);
+        const chunkOffset = Math.floor(index / 32);
+        const offset = index - chunkOffset * 32;
+        if (value) {
+            this.buffer[chunkOffset] |= (1 << offset);
+        } else {
+            this.buffer[chunkOffset] &= ~(1 << offset);
+        }
+        return this;
+    }
+
+    private validateIndex(index:number) {
+        if (index > this.length - 1) {
+            throw new RangeError('Index is too large.');
+        }
+    }
+
+    private bitsCountInNumber (value:number) {
+        value -= ((value >>> 1) & 0x55555555);
+        value = (value & 0x33333333) + ((value >>> 2) & 0x33333333);
+        return (((value + (value >>> 4) & 0xF0F0F0F) * 0x1010101) >>> 24);
+    }
+}
