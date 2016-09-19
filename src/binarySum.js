5dfcb514ca527374fccbb2044642f48455431ce9
Yordan Stoev <sularome>
2016-09-19 23:15:27 +0200

    Fix binary sum

--- src/binarySum.js
+++ src/binarySum.js
@@ -2,10 +2,13 @@ function binarySum(A, B) {
     var i = 0;
     var n = A.length;
     var C = [];
+    A = A.slice(0).reverse();
+    B = B.slice(0).reverse();
     while (i < n) {
         let carryOn = C[i];
         C[i] = A[i] ^ B[i] ^ carryOn;
         C[i + 1] = (A[i] & B[i]) | (A[i] & carryOn) | (B[i] & carryOn);
+        i++;
     }
-    return C;
+    return C.reverse();
 }
