4768534abc71dc9fd7cbec0c42c61d77c4496fa1
yordan <Jordan2384>
2016-10-20 19:52:56 +0200

    add search to BinaryTree

--- tests/BinaryTreeSpec.js
+++ tests/BinaryTreeSpec.js
@@ -1,9 +1,9 @@
 describe('BinaryTree', function () {
     beforeAll(function (done) {
-        System.import('BinaryTree').then((a) => {
+        System.import('BinaryTree').then(function (a) {
             this.BinaryTree = a.BinaryTree;
             done();
-        });
+        }.bind(this));
     });
     it('should be able to add node', function () {
         var tree = new this.BinaryTree(function (a,b) {return a > b;});
@@ -13,6 +13,17 @@ describe('BinaryTree', function () {
         expect(tree.root.right.value).toEqual(6);
         tree.add(4);
         expect(tree.root.left.value).toEqual(4);
+        var ar = [];
+    });
+
+    it('should be able to add node to binary tree with object values', function () {
+        var tree = new this.BinaryTree(function (a,b) {return a.amount > b.amount;});
+        tree.add({amount : 5});
+        expect(tree.root.value).toEqual({amount : 5});
+        tree.add({amount : 6});
+        expect(tree.root.right.value).toEqual({amount : 6});
+        tree.add({amount : 4});
+        expect(tree.root.left.value).toEqual({amount : 4});
         var ar = [];
     });
 
@@ -29,6 +40,20 @@ describe('BinaryTree', function () {
         }, [])).toEqual([1,2,4,5,6]);
     });
 
+    it('should be able to walk the binary tree that has object values in order', function () {
+        var tree = new this.BinaryTree(function (a,b) {return a.amount > b.amount;});
+        tree.add({amount: 6});
+        tree.add({amount: 4});
+        tree.add({amount: 5});
+        tree.add({amount: 1});
+        tree.add({amount: 2});
+        expect(tree.inorderTreeWalk(function (pv, cv) {
+            pv.push(cv.amount);
+            return pv;
+        }, [])).toEqual([1,2,4,5,6]);
+    });
+
+
     it('should be able to walk the binary tree in order', function () {
         var tree = new this.BinaryTree(function (a,b) {return a > b;});
         tree.add(5);
@@ -39,6 +64,42 @@ describe('BinaryTree', function () {
         expect(tree.reverseTreeWalk(function (pv, cv) {
             pv.push(cv);
             return pv;
-        }, [])).toEqual([1,2,4,5,6].reverse());
+        }, [])).toEqual([6,5,4,2,1]);
+    });
+
+    it('should be able to walk the binary tree in reverseorder', function () {
+        var tree = new this.BinaryTree(function (a,b) {return a.amount > b.amount;});
+        tree.add({amount: 6});
+        tree.add({amount: 4});
+        tree.add({amount: 5});
+        tree.add({amount: 1});
+        tree.add({amount: 2});
+        expect(tree.reverseTreeWalk(function (pv, cv) {
+            pv.push(cv.amount);
+            return pv;
+        }, [])).toEqual([6,5,4,2,1]);
+    });
+
+    describe(' search', function () {
+        it('should return null when no root', function () {
+            var tree = new this.BinaryTree(function (a,b) {return a > b;});
+            expect(tree.search(5)).toBeNull();
+        });
+
+        it('should return element when found', function () {
+            var tree = new this.BinaryTree(function (a,b) {return a > b;});
+            tree.add(5);
+            expect(tree.search(5).value).toEqual(5);
+            tree.add(6);
+            expect(tree.search(6).value).toEq6ual(6);
+        });
+
+        it('should return null when no element was found', function () {
+            var tree = new this.BinaryTree(function (a,b) {return a > b;});
+            tree.add(5);
+            expect(tree.search(23)).toBeNull();
+            tree.add(6);
+            expect(tree.search(16)).toBeNull();
+        });
     });
 });
--- ts/BinaryTree.ts
+++ ts/BinaryTree.ts
@@ -1,6 +1,6 @@
 export class BinaryTree<T>{
     public comparator:(a:T, b:T) => boolean;
-    public root:BinaryTreeNode<T> = new BinaryTreeNode<T>(null);
+    public root:BinaryTreeNode<T> = new BinaryTreeNode<T>(null, null);
 
     constructor(comparator:(a:T, b:T) => boolean) {
         this.comparator = comparator;
@@ -19,11 +19,24 @@ export class BinaryTree<T>{
             }
 
             if (this.comparator(parent.value, value)) {
-                parent.left = new BinaryTreeNode<T>(value);
+                parent.left = new BinaryTreeNode<T>(value, parent);
             } else {
-                parent.right = new BinaryTreeNode<T>(value);
+                parent.right = new BinaryTreeNode<T>(value, parent);
             }
         }
+    }
+
+    public search(value:T) {
+        var currentNode:BinaryTreeNode<T> = this.root;
+        while(currentNode && currentNode.value !== value) {
+            if (this.comparator(currentNode.value, value)) {
+                currentNode = currentNode.left;
+            } else {
+                currentNode = currentNode.right;
+            }
+        }
+
+        return currentNode;
     }
 
     private getNextChild(node:BinaryTreeNode<T>, value:T):BinaryTreeNode<T> {
@@ -61,8 +74,10 @@ export class BinaryTreeNode<T>{
     public left:BinaryTreeNode<T> = null;
     public right:BinaryTreeNode<T> = null;
     public value:T;
+    public parent:BinaryTreeNode<T> = null;
 
-    constructor(value:T) {
+    constructor(value:T, parent: BinaryTreeNode<T>) {
         this.value = value;
+        this.parent = parent;
     }
 }
