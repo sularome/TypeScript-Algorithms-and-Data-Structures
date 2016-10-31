d56eb4fa1d4fa0ef5fef553bbf9bdbbf9d082f1b
yordan <sularome@gmail.com>
2016-10-31 10:46:19 +0100

    remove comments from karma config file and require setup file

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,5 @@
-// Karma configuration
-// Generated on Mon Jun 06 2016 11:19:28 GMT+0200 (Central European Daylight Time)
-
 module.exports = function(config) {
   var configuration = {
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
     customLaunchers: {
       Chrome_travis_ci: {
@@ -17,21 +12,12 @@ module.exports = function(config) {
         dir : 'coverage/',
         subdir: '.'
     },
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine', 'requirejs'],
-
-
-    // list of files / patterns to load in the browser
     files: [
         {pattern: 'build/**/*.js', included: false},
         {pattern: 'tests/test-main.js'}
 
     ],
-
-
-    // list of files to exclude
     exclude: [
     ],
 
@@ -41,49 +27,16 @@ module.exports = function(config) {
         'karma-jasmine',
         'karma-chrome-launcher'
     ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
         './build/**/*.js': 'coverage'
     },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress', 'coverage'],
-
-
-    // web server port
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_ERROR,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
     singleRun: true,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
     concurrency: Infinity
   };
 
--- tests/test-main.js
+++ tests/test-main.js
@@ -1,15 +1,7 @@
 var TEST_REGEXP = /^\/base\/build/i;
 var allTestFiles = [];
 
-// Get a list of all the test files to include
 Object.keys(window.__karma__.files).forEach(function(file) {
-    // if (TEST_REGEXP.test(file)) {
-    //     // Normalize paths to RequireJS module names.
-    //     // If you require sub-dependencies of test files to be loaded as-is (requiring file extension)
-    //     // then do not normalize the paths
-    //     var normalizedTestModule = file.replace(/^\/base\/|\.js$/g, '');
-    //     allTestFiles.push(normalizedTestModule);
-    // }
     if (TEST_REGEXP.test(file)) {
         var normalizedTestModule = file.replace(/^\/base\/|\.js$/g, '');
         allTestFiles.push(normalizedTestModule);
@@ -17,20 +9,11 @@ Object.keys(window.__karma__.files).forEach(function(file) {
 });
 
 require.config({
-    // Karma serves files under /base, which is the basePath from your config file
     baseUrl: '/base',
-
-    // example of using a couple of path translations (paths), to allow us to refer to different library dependencies, without using relative paths
     paths: {
     },
-
-    // example of using a shim, to load non AMD libraries (such as underscore)
     shim: {
     },
-
-    // dynamically load all test files
     deps: allTestFiles,
-
-    // we have to kickoff jasmine, as it is asynchronous
     callback: window.__karma__.start
 });
