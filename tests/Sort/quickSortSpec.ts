7b9ae090fec34505b29f9b6c5d7e0269b9d83136
yordan <sularome@gmail.com>
2017-06-03 21:31:15 +0200

    quick sort

---
+++ tests/Sort/quickSortSpec.ts
@@ -0,0 +1,28 @@
+import quickSort from "../../ts/Sort/quickSort";
+describe("quicksort sort", function () {
+    it("should sort", function () {
+        expect(quickSort([4, 1])).toEqual([1, 4]);
+        expect(quickSort([4, 1, 3])).toEqual([1, 3, 4]);
+        expect(quickSort([1, 4, 2, 3])).toEqual([1, 2, 3, 4]);
+        expect(quickSort([11, 4, 2, 3])).toEqual([2, 3, 4, 11]);
+        expect(quickSort([11, 4, 2])).toEqual([2, 4, 11]);
+        expect(quickSort([11, 4, 2, 3, 5])).toEqual([2, 3, 4, 5, 11]);
+        expect(quickSort([5, 2, 4, 6, 1, 3])).toEqual([1, 2, 3, 4, 5, 6]);
+    });
+
+    it("should return empty array if empty or one element array is passed", function () {
+        expect(quickSort([])).toEqual([]);
+        expect(quickSort([1])).toEqual([1]);
+    });
+
+    it("should reverse  sort", function () {
+        expect(quickSort([1, 4, 2, 3], (a, b) => a < b)).toEqual([4, 3, 2, 1]);
+        expect(quickSort([11, 4, 2, 3], (a, b) => a < b)).toEqual([11, 4, 3, 2]);
+        expect(quickSort([5, 2, 4, 6, 1, 3], (a, b) => a < b)).toEqual([6, 5, 4, 3, 2, 1]);
+    });
+
+    it("should work for any kind of objects", function () {
+        expect(quickSort(["a", "b", "a", "C", "d"], (a, b) => a > b)).toEqual(["C", "a", "a", "b", "d"]);
+        expect(quickSort([{id: 5}, {id: 4}, {id: 3}, {id: 2}], (a, b) => a.id > b.id)).toEqual([{id: 2}, {id: 3}, {id: 4}, {id: 5}]);
+    });
+});
