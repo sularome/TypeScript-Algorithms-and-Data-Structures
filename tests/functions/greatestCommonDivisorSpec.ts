b51c4b040538bf7c827ead552c7740374599f084
Yordan Stoev <yordan.stoev@vistex.com>
2019-01-11 15:07:56 +0100

    Improve ede cases for Greatest common divisor algorithm

--- tests/functions/greatestCommonDivisorSpec.ts
+++ tests/functions/greatestCommonDivisorSpec.ts
@@ -2,18 +2,27 @@ import { greatestCommonDivisor } from "../../ts/functions/greatestCommonDivisor"
 
 describe("greatestCommonDivisor", () => {
     it("should be able to find common divisor for the same number", () => {
-        expect(greatestCommonDivisor(17, 17)).toEqual(17)
+        expect(greatestCommonDivisor(17, 17)).toEqual(17);
     });
     it("should return 1 when no common divisor", () => {
-        expect(greatestCommonDivisor(17, 6)).toEqual(1)
+        expect(greatestCommonDivisor(17, 6)).toEqual(1);
     });
     it("should return first if it is multiple of second", () => {
-        expect(greatestCommonDivisor(15, 35)).toEqual(5)
+        expect(greatestCommonDivisor(15, 35)).toEqual(5);
     });
-    it("should throw if first parameter is less than 0", () => {
-        expect(() => greatestCommonDivisor(-15, 35)).toThrow()
+    it("should return NaN if first parameter is less than 0", () => {
+        expect(greatestCommonDivisor(-15, 35)).toBeNaN();
     });
-    it("should throw if second parameter is less than 0", () => {
-        expect(() => greatestCommonDivisor(15, -35)).toThrow()
+    it("should return NaN if second parameter is less than 0", () => {
+        expect(greatestCommonDivisor(15, -35)).toBeNaN();
+    });
+    it("should return NaN if both parameters are 0", () => {
+        expect(greatestCommonDivisor(0, 0)).toBeNaN();
+    });
+    it("should return second parameter if first is 0", () => {
+        expect(greatestCommonDivisor(3, 0)).toEqual(3);
+    });
+    it("should return first parameter if second is 0", () => {
+        expect(greatestCommonDivisor(0, 4)).toEqual(4);
     });
 });
--- ts/functions/greatestCommonDivisor.ts
+++ ts/functions/greatestCommonDivisor.ts
@@ -1,14 +1,11 @@
 /**
  * Euclid's algorithm for finding the greatest common divisor of two numbers greater than 0
  * @param {number} a greater than 0
- * @param {number} b greater than 0 
+ * @param {number} b greater than 0
  */
 export function greatestCommonDivisor(a: number, b: number): number {
-    if (a <= 0) {
-        throw new Error(`Parametter less or equal than zero. ${a}`)
-    }
-    if (b < 0) {
-        throw new Error(`Parametter less or equal than zero. ${b}`)
+    if (a < 0 || b < 0 || a + b === 0) {
+        return NaN;
     }
     if (a < b) {
         return greatestCommonDivisor(b, a);
