0339c49f9ffe38443a4b0bc6d08d4514953199fe
yordan <sularome@gmail.com>
2019-01-12 22:08:34 +0100

    Added Extended Euclide's Algorithm

---
+++ ts/functions/extendedEuclidesAlgorithm.ts
@@ -0,0 +1,46 @@
+import { IExtendedEuclidesAlgorithmResult } from "../Interfaces/IExtendedEuclidesAlgorithmResult";
+
+/**
+ * Extended Euclid's algorithm for finding the greatest common divisor of two numbers greater than 0 and its polynomial prepresentation
+ * @param {number} a greater than 0
+ * @param {number} b greater than 0
+ */
+export function extendedEuclidesAlgorithm(a: number, b: number): IExtendedEuclidesAlgorithmResult {
+    if (Math.round(a) !== a || Math.round(b) !== b) {
+        throw new Error("Euclide's extended algorithm works only for positive integers");
+    }
+    if (a < 0 || b < 0 || a + b === 0) {
+        throw new Error("Euclide's extended algorithm works only for positive integers");
+    }
+    if (a === b) {
+        return {
+            gcd: a,
+            x: 1,
+            y: 0
+        };
+    } else if (a < b) {
+        const result = extendedEuclidesAlgorithmInternal(b, a);
+        return {
+            gcd: result.gcd,
+            x: result.y,
+            y: result.x
+        }
+    } else {
+        return  extendedEuclidesAlgorithmInternal(a, b);
+    }
+}
+
+function extendedEuclidesAlgorithmInternal(a: number, b: number): IExtendedEuclidesAlgorithmResult {
+    const result: IExtendedEuclidesAlgorithmResult = {
+        gcd: a,
+        x: 1,
+        y: 0
+    };
+    if (b !== 0) {
+        const recursiveResult = extendedEuclidesAlgorithmInternal(b, a % b);
+        result.gcd = recursiveResult.gcd;
+        result.x = recursiveResult.y;
+        result.y = recursiveResult.x - recursiveResult.y * Math.floor(a / b);
+    }
+    return  result;
+}
